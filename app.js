567c56466bd8fd2b3e2a56bd2c1139b0c0a87142
Kevin <kevinbanas@gmail.com>
2017-05-16 20:02:29 -0500

    adding chess.js library

--- app.js
+++ app.js
@@ -1,1 +1,10 @@
-alert('hi');
+var Chess = require('./node_modules/chess.js/chess').Chess;
+var chess = new Chess();
+
+chess.move('e4');
+chess.move('e6');
+chess.move('d4');
+chess.move('d5');
+var h = chess.fen();
+
+console.log(h);
