5b2d663ef9ada3c19b4397030a0a982b358d5b1c
Kevin <kevinbanas@gmail.com>
2017-05-16 22:56:23 -0500

    reading files

--- app.js
+++ app.js
@@ -1,10 +1,30 @@
 var Chess = require('./node_modules/chess.js/chess').Chess;
 var chess = new Chess();
+var fs = require('file-system');
 
-chess.move('e4');
-chess.move('e6');
-chess.move('d4');
-chess.move('d5');
-var h = chess.fen();
+var myPositions = [];
+var myFiles = [];
 
-console.log(h);
+function processFile() {
+    console.log(myPositions);
+}
+
+function collectFiles() {
+    console.log(myFiles)
+}
+
+fs.readFile('./fens/french-advance.txt', function read(err, data) {
+    if (err) {
+        throw err;
+    }
+    myPositions.push(data);
+    processFile();
+});
+
+fs.readdir('./fens', function read(err, files) {
+    if (err) {
+        throw err;
+    }
+    myFiles = files;
+    collectFiles();
+})
